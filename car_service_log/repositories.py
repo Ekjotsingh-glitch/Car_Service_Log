511e9e7304edd3594dae0d2533888db4a8ada9e6
Ekjot Singh <ekjotsingh213@outlook.com>
2025-12-25 17:52:42 +0100

    Adding ServiceRepo with add_service and all_services

--- car_service_log/repositories.py
+++ car_service_log/repositories.py
@@ -31,3 +31,24 @@ class CarRepository:
         return row[0]
 
 
+class ServiceRepository:
+    def __init__(self, db):
+        self.db = db
+
+    def add_service(self, car_id, date, mileage, service_type, cost, notes):
+        self.db.cur.execute("""
+            INSERT INTO service_records (car_id, date, mileage, service_type, cost, notes)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, (car_id, date, mileage, service_type, cost, notes))
+        self.db.conn.commit()
+
+    def all_services(self):
+        self.db.cur.execute("""
+        SELECT sr.id, c.plate, sr.date, sr.mileage, sr.service_type, sr.cost
+        FROM service_records sr
+        JOIN cars c ON c.id = sr.car_id
+        ORDER BY sr.date DESC
+        """)
+        return self.db.cur.fetchall()
+    
+   
